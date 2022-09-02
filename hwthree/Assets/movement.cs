f5370035c11612f2ea096cb5ec57bad6b72fac8c
NawafAlafasi <nawafalmutairi2005@gmail.com>
2022-09-03 02:09:46 +0300

    done

--- hwthree/Assets/movement.cs
+++ hwthree/Assets/movement.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-
+    public Transform groundPos;
     Rigidbody2D rb;
-    
+    public LayerMask groundLayer;
+    public float rad;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,8 @@ public class movement : MonoBehaviour
 
     void move()
     {
+        bool isGrounded = Physics2D.OverlapCircle(groundPos.position, rad, groundLayer);
+
         if (Input.GetKeyDown(KeyCode.A))
         {
             rb.velocity = new Vector2(-4, 0f);
@@ -30,9 +33,9 @@ public class movement : MonoBehaviour
         {
             rb.velocity = new Vector2(4, 0f);
         }
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
-            rb.velocity = new Vector2(0f, 4);
+            rb.velocity = new Vector2(0f, 12);
         }
     }
 }
